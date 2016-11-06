7a89730b0688a7ae04f5a688d154a685301799a2
insaider <insaider@bk.ru>
2016-11-06 21:52:02 +1000

    Добавил решение задания 2.2.3

---
+++ chapter_002/part_002/src/main/java/ru/skorikov/StartUI.java
@@ -0,0 +1,11 @@
+package ru.skorikov;
+
+public class StartUI{
+	public Tracker tracker = new Tracker();
+	
+	
+	public static void main(String[] args){
+		System.out.println("Start programms");
+	}
+	
+}
