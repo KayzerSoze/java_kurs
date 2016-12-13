916e2a8c7cf4df5cd8ef6ffaa3f2d942e9581ede
KayzerSoze <insaider@bk.ru>
2016-12-13 10:05:28 +1000

    test home-work

--- chapter_002/part_005/src/main/java/ru/skorikov/ConsoleInput.java
+++ chapter_002/part_005/src/main/java/ru/skorikov/ConsoleInput.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
  * задает вопрос и получает ответ пользователя
  *
  * @author Alexey Skorikov
- * @version 1.0
+ * @version 2.0
  * @since 13.11.2016
  */
 public class ConsoleInput implements Input {
@@ -17,7 +17,7 @@ public class ConsoleInput implements Input {
 
     /**
      * Метод выводит Вопрос.
-     * и ожидает Щтвет от пользователя
+     * и ожидает Ответ от пользователя
      *
      * @param question вопрос
      * @return ответ
@@ -26,4 +26,17 @@ public class ConsoleInput implements Input {
         System.out.println(question);
         return scanner.nextLine();
     }
+    /**
+     * Метод выводит вопрос.
+     * ожидает ответ пользователя
+     * 
+     * @param question вопрос
+     * @param range диапазон
+     * @return ответ
+     */
+    public String ask(String question, int range) {
+        System.out.println(question);
+        return scanner.nextLine();
+    }
+
 }
