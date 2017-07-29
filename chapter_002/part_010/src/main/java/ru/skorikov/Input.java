43be2b4325d8819ad29beaa24dd82f90eb2bb143
insaider <insaider@bk.ru>
2017-07-29 16:18:38 +1000

    ООП - Анонимные и абстрактные классы: Нужно сделать абстрактный класс BaseAction с конструктором.

---
+++ chapter_002/part_010/src/main/java/ru/skorikov/Input.java
@@ -0,0 +1,28 @@
+package ru.skorikov;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @ author: Alex Skorikov.
+ * @ date: 10.05.17
+ * @ version Stady_2.
+ */
+public interface Input {
+    /**
+     * Метод полчает парметром строку.
+     *
+     * @param question выводит вопрос.
+     * @return возвращает ответ пользователя.
+     */
+    String ask(String question);
+    /**
+     * Метод получает параметром строку и диапазон значений.
+     *
+     * @param question выводит вопрос.
+     * @param rahge    выбирает из диапазона.
+     * @return ответ.
+     * @throws MenuOutException сообщение о ошибке.
+     */
+    int ask(String question, int[] rahge) throws MenuOutException;
+
+}
---
+++ chapter_002/part_010/src/main/java/ru/skorikov/StartUI.java
@@ -0,0 +1,78 @@
+package ru.skorikov;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @ author: Alex Skorikov.
+ * @ date: 03.05.17
+ * @ version Stady_2.
+ */
+class StartUI {
+    /**
+     * Диапазон значений меню.
+     */
+    private int[] range = new int[MenuTracker.RAZMER];
+
+    {
+        for (int i = 0; i < range.length; i++) {
+            range[i] = i;
+        }
+    }
+
+    /**
+     * Интерфейс.
+     * Переменная класса
+     */
+    private Input input;
+    /**
+     * Поле класса.
+     * Переменная tracker.
+     */
+    private Tracker tracker;
+
+    /**
+     * Конструктор класса.
+     *
+     * @param input   интерфейс.
+     * @param tracker Трекер - хранишище заявок.
+     */
+    protected StartUI(Input input, Tracker tracker) {
+        this.input = input;
+        this.tracker = tracker;
+    }
+
+    /**
+     * Метод класса.
+     *
+     * @throws MenuOutException сообщение о ошибке.
+     */
+    public void init() throws MenuOutException {
+        MenuTracker menu = new MenuTracker(this.input, this.tracker);
+        menu.fillActions();
+        do {
+            menu.show();
+            menu.select(input.ask("Select:", range));
+        } while ((!"y".equals(this.input.ask("Exit: ? y/n"))));
+    }
+
+    /**
+     * Метод запуска приложения.
+     *
+     * @param args массив строк.
+     * @throws MenuOutException сообщение о ошибке.
+     */
+    public static void main(String[] args) throws MenuOutException {
+        /**
+         * Интерфейс Input реализуем как ConsoleInput.
+         */
+        Input input = new ValidateInput();
+        /**
+         * Создаем новый объект класса Tracker.
+         */
+        Tracker tracker = new Tracker();
+        /**
+         * StartUI принимает input, tracker и запускает метод init.
+         */
+        new StartUI(input, tracker).init();
+    }
+}
